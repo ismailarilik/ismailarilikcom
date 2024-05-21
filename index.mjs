5a9ce1b199f5f997bdaaf89baab5cf3f094f6b41
İsmail Arılık <arilik.ismail@gmail.com>
2024-05-21 17:51:48 +0300

    Render HTML instead of plain text

--- index.mjs
+++ index.mjs
@@ -1,12 +1,20 @@
 import { createServer } from 'node:http';
+import fs from 'node:fs';
 
 const hostname = '0.0.0.0';
 const port = 80;
 
 const server = createServer((req, res) => {
   res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
+  res.setHeader('Content-Type', 'text/html');
+
+  fs.readFile('index.html', 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+    } else {
+      res.end(data);
+    }
+  });
 });
 
 server.listen(port, hostname, () => {
